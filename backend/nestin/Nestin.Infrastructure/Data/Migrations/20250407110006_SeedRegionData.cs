91347719d6184c10ddb411b256111e74786b3f0f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 13:01:16 +0200

    feat(backend): seed region data

---
+++ backend/nestin/Nestin.Infrastructure/Data/Migrations/20250407110006_SeedRegionData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Nestin.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRegionData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "North America" },
+                    { 2, "Europe" },
+                    { 3, "Asia" },
+                    { 4, "South America" },
+                    { 5, "Africa & Oceania" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
