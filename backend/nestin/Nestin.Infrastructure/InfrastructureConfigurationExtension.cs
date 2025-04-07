aaaa7b74d489e51946c8e422dd8bcd0eabc85ba1
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 11:04:53 +0200

    feat(backend): add infra DI extension method

---
+++ backend/nestin/Nestin.Infrastructure/InfrastructureConfigurationExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure
+{
+    public static class InfrastructureConfigurationExtension
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options
+                    => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
