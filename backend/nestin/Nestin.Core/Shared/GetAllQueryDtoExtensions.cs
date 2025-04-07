6b28b2aec56f55ffb4b6db3a89486209a3acc2f5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 12:19:54 +0200

    feat(backend): add GetAllQueryDto

---
+++ backend/nestin/Nestin.Core/Shared/GetAllQueryDtoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Nestin.Core.Dtos;
+
+namespace Nestin.Core.Shared
+{
+    public static class GetAllQueryDtoExtensions
+    {
+        public static int CalcSkippedItems(this GetAllQueryDto dto)
+        {
+            return (dto.Page - 1) * dto.PageSize;
+        }
+    }
+}
