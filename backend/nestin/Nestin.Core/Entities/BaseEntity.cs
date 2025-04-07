a19012f009b74366b6f5346d9419fdc557c9ba2f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 11:21:42 +0200

    feat(backend): add base entity in core proj

---
+++ backend/nestin/Nestin.Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Nestin.Core.Entities
+{
+    /// <summary>
+    /// Base Entity for Entities in our system 
+    /// </summary>
+    /// <typeparam name="T">Represents type of entity Id (`string` or `int`)</typeparam>
+    public class BaseEntity<T>
+    {
+        public T Id { get; set; }
+    }
+}
