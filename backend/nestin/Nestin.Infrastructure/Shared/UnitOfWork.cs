3390a475531e3c0b42703874e138e8a424928bac
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 12:28:58 +0200

    feat(backend): add UnitOfWork

---
+++ backend/nestin/Nestin.Infrastructure/Shared/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Nestin.Core.Interfaces;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure.Shared
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
