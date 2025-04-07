38e02c4cdfcecd9cc366404ff4c327f03e73d1a4
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 13:12:23 +0200

    feat(backend): add region repository

---
+++ backend/nestin/Nestin.Core/Interfaces/IRegionRepository.cs
@@ -0,0 +1,8 @@
+﻿using Nestin.Core.Entities;
+
+namespace Nestin.Core.Interfaces
+{
+    public interface IRegionRepository : IGenericRepository<Region, int>
+    {
+    }
+}
---
+++ backend/nestin/Nestin.Infrastructure/Repositories/RegionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Nestin.Core.Entities;
+using Nestin.Core.Interfaces;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure.Repositories
+{
+    public class RegionRepository : GenericRepository<Region, int>, IRegionRepository
+    {
+        public RegionRepository(AppDbContext dbContext) : base(dbContext) { }
+    }
+}
