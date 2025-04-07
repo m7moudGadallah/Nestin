94fdf35673975563363650b555240ce9d41a7563
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 13:06:59 +0200

    feat(backend): add BaseController

---
+++ backend/nestin/Nestin.Api/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nestin.Core.Interfaces;
+
+namespace Nestin.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected virtual NotFoundObjectResult NotFoundResponse(string? message = null)
+        {
+            return new NotFoundObjectResult(new List<string>
+            {
+                message ?? "Resource not found!"
+            });
+        }
+    }
+}
