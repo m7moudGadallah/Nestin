bcbe8fa5f4325f11fd998e2b6bf075623a20accd
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 12:20:54 +0200

    feat(backend): add IGenericRepository

---
+++ backend/nestin/Nestin.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Nestin.Core.Dtos;
+using Nestin.Core.Entities;
+using Nestin.Core.Shared;
+using System.Linq.Expressions;
+
+namespace Nestin.Core.Interfaces
+{
+    public interface IGenericRepository<TEntity, T> where TEntity : BaseEntity<T>
+    {
+        public Task<PaginatedResult<TEntity>> GetAllAsync(GetAllQueryDto queryDto);
+        public Task<PaginatedResult<TEntity>> GetAllAsync(GetAllQueryDto queryDto, params Expression<Func<TEntity, object>>[] includes);
+        public Task<TEntity?> GetByIdAsync(T id);
+        public Task<TEntity?> GetByIdAsync(T id, params Expression<Func<TEntity, object>>[] includes);
+        public void Create(TEntity entity);
+        public void Update(TEntity entity);
+        public Task DeleteAsync(T id);
+        public Task SaveChangesAsync();
+    }
+}
