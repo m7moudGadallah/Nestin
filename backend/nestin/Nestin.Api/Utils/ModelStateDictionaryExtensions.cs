f8fe057065bc352332dc205a92a7976387aaa738
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 12:34:55 +0200

    feat(backend): add ModelStateDictionaryExtensions

---
+++ backend/nestin/Nestin.Api/Utils/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Nestin.Api.Utils
+{
+    public static class ModelStateDictionaryExtensions
+    {
+        public static List<string> ExtractErrorList(this ModelStateDictionary model)
+        {
+            return model.Where(kv => kv.Value.Errors.Any()) // Only include fields with errors
+                .SelectMany(kv => kv.Value.Errors.Select(e => e.ErrorMessage)) // Extract error messages
+                .ToList();
+        }
+    }
+}
