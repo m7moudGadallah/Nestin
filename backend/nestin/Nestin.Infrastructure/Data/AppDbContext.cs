f27beab703c9f8762eb463072ed32d9b875fc8db
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 10:51:44 +0200

    feat(backend): add AppDbContext

---
+++ backend/nestin/Nestin.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Nestin.Infrastructure.Data
+{
+    class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
