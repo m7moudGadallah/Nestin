4deb26e77201f7502446c787b74a4fdc80a8bab5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-07 11:56:20 +0200

    feat(backend): add paginated result types

---
+++ backend/nestin/Nestin.Core/Shared/PaginatedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Nestin.Core.Shared
+{
+    /// <summary>
+    /// Represents Paginated Result will be returned from Repositories
+    /// </summary>
+    /// <typeparam name="TEntity">Entity Type</typeparam>
+    public class PaginatedResult<TEntity>
+    {
+        public IEnumerable<TEntity> Items { get; set; }
+        public PaginationMetaData MetaData { get; set; }
+    }
+}
---
+++ backend/nestin/Nestin.Core/Shared/PaginationMetaData.cs
@@ -0,0 +1,12 @@
+﻿namespace Nestin.Core.Shared
+{
+    /// <summary>
+    /// Represents Pagination Meta data will returned from Repositories.
+    /// </summary>
+    public class PaginationMetaData
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int Total { get; set; }
+    }
+}
