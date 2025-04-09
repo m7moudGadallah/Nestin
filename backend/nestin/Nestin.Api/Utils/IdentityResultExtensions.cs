c7d706d3a8a9c0cf63eca72202e150581651d56f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-09 14:16:42 +0200

    feat(backend): add IdentityResultExtensions

---
+++ backend/nestin/Nestin.Api/Utils/IdentityResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Nestin.Api.Utils
+{
+    public static class IdentityResultExtensions
+    {
+        public static List<string> ToErrorList(this IdentityResult result)
+        {
+            return result.Errors.Select(e => $"{e.Code}: {e.Description}").ToList();
+        }
+    }
+}
