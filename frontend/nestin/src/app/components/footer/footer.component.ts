49be5c76ad22a302f13cf8b0886a49a4475ce4df
rana12-ux <rana.ahmedelsawy@gmail.com>
2025-04-11 19:17:30 -0700

    added homepage design

---
+++ frontend/nestin/src/app/components/footer/footer.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  @ViewChild('scrollable') scrollable!: ElementRef;
+
+  showLeft = false;
+  showRight = false;
+  
+  ngAfterViewInit() {
+    setTimeout(() => this.checkScroll(), 100);
+  }
+  
+  scrollLeft() {
+    this.scrollable.nativeElement.scrollBy({ left: -200, behavior: 'smooth' });
+  }
+  
+  scrollRight() {
+    this.scrollable.nativeElement.scrollBy({ left: 200, behavior: 'smooth' });
+  }
+  
+  checkScroll() {
+    const scrollEl = this.scrollable.nativeElement;
+    this.showLeft = scrollEl.scrollLeft > 0;
+    this.showRight = scrollEl.scrollLeft + scrollEl.clientWidth < scrollEl.scrollWidth;
+  }
+  
+}
