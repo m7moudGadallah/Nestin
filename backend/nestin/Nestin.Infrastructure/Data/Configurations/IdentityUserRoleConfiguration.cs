5f5ed3e29fe419b304569fcc769f215baaa84ecb
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-14 19:54:23 +0200

    feat(backend): seed UserRoles data

---
+++ backend/nestin/Nestin.Infrastructure/Data/Configurations/IdentityUserRoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nestin.Infrastructure.Data.Seeds;
+
+namespace Nestin.Infrastructure.Data.Configurations
+{
+    class IdentityUserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(IdentityUserRoleSeed.Data);
+        }
+    }
+}
---
+++ backend/nestin/Nestin.Infrastructure/Data/Seeds/IdentityUserRoleSeed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Nestin.Infrastructure.Data.Seeds
+{
+    static class IdentityUserRoleSeed
+    {
+        public static List<IdentityUserRole<string>> Data => new()
+        {
+            new IdentityUserRole<string>
+            {
+                UserId = "2dacdb51-fee9-4479-904c-cafe7dca22a6", // admin
+                RoleId = "d35a86a5-72b3-4e7e-bb7f-5ef782b36f7c"  // Admin role ID
+            },
+            new IdentityUserRole<string>
+            {
+                UserId = "3dacdb51-fee9-4479-904c-cafe7dca22a7", // host
+                RoleId = "59ebef1f-d79b-4db0-9c5a-304836f14ff1"  // Host role ID
+            },
+            new IdentityUserRole<string>
+            {
+                UserId = "4dacdb51-fee9-4479-904c-cafe7dca22a8", // guest
+                RoleId = "9c75a5df-20a4-4ff1-85a5-bb52f9cf223f"  // Guest role ID
+            }
+        };
+    }
+}
