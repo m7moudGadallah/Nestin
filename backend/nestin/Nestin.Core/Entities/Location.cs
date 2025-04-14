6360749c8703b5ca3ad92bcd444f749c36c9daad
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-14 04:56:30 +0200

    feat(backend): add Location entity

---
+++ backend/nestin/Nestin.Core/Entities/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace Nestin.Core.Entities
+{
+    public class Location : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
+    }
+}
