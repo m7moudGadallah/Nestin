c05dee54c21bf6c54b535d5f85c12575564c1d9d
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-14 13:33:20 +0200

    feat(backend): seed PropertySpaceTypes data

---
+++ backend/nestin/Nestin.Infrastructure/Data/Migrations/20250414113234_SeedPropertySpaceTypesData.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Nestin.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedPropertySpaceTypesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PropertySpaceTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Bedroom" },
+                    { 2, "Bathroom" },
+                    { 3, "Kitchen" },
+                    { 4, "Living Room" },
+                    { 5, "Dining Area" },
+                    { 6, "Workspace" },
+                    { 7, "Laundry Area" },
+                    { 8, "Private Entrance" },
+                    { 9, "Balcony" },
+                    { 10, "Patio" },
+                    { 11, "Backyard" },
+                    { 12, "Fire Pit" },
+                    { 13, "Baby Room" },
+                    { 14, "Children’s Play Area" },
+                    { 15, "Closet" },
+                    { 16, "Storage Space" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "PropertySpaceTypes",
+                keyColumn: "Id",
+                keyValue: 16);
+        }
+    }
+}
---
+++ backend/nestin/Nestin.Infrastructure/Data/Seeds/PropertySpaceTypeSeed.cs
@@ -0,0 +1,27 @@
+﻿using Nestin.Core.Entities;
+
+namespace Nestin.Infrastructure.Data.Seeds
+{
+    static class PropertySpaceTypeSeed
+    {
+        public static List<PropertySpaceType> Data => new()
+        {
+            new PropertySpaceType { Id = 1, Name = "Bedroom" },
+            new PropertySpaceType { Id = 2, Name = "Bathroom" },
+            new PropertySpaceType { Id = 3, Name = "Kitchen" },
+            new PropertySpaceType { Id = 4, Name = "Living Room" },
+            new PropertySpaceType { Id = 5, Name = "Dining Area" },
+            new PropertySpaceType { Id = 6, Name = "Workspace" },
+            new PropertySpaceType { Id = 7, Name = "Laundry Area" },
+            new PropertySpaceType { Id = 8, Name = "Private Entrance" },
+            new PropertySpaceType { Id = 9, Name = "Balcony" },
+            new PropertySpaceType { Id = 10, Name = "Patio" },
+            new PropertySpaceType { Id = 11, Name = "Backyard" },
+            new PropertySpaceType { Id = 12, Name = "Fire Pit" },
+            new PropertySpaceType { Id = 13, Name = "Baby Room" },
+            new PropertySpaceType { Id = 14, Name = "Children’s Play Area" },
+            new PropertySpaceType { Id = 15, Name = "Closet" },
+            new PropertySpaceType { Id = 16, Name = "Storage Space" }
+        };
+    }
+}
