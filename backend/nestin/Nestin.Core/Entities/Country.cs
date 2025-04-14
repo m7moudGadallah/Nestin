051f6cf821b4d9c3d27ea8762b24deed83ae3f68
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-14 04:31:32 +0200

    feat(backend): add Country entity

---
+++ backend/nestin/Nestin.Core/Entities/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace Nestin.Core.Entities
+{
+    public class Country : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public int RegionId { get; set; }
+        public virtual Region Region { get; set; }
+    }
+}
