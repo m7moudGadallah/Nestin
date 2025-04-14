ae7a94d5efd27d6b60dcdd7b01970ca6634c04c0
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-14 10:53:33 +0200

    feat(backend): seed GuestTypes data

---
+++ backend/nestin/Nestin.Infrastructure/Data/Migrations/20250414084336_SeedGuestTypesData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Nestin.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGuestTypesData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GuestTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Adults" },
+                    { 2, "Childern" },
+                    { 3, "Infants" },
+                    { 4, "Pets" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GuestTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "GuestTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "GuestTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "GuestTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
---
+++ backend/nestin/Nestin.Infrastructure/Data/Seeds/GuestTypeSeed.cs
@@ -0,0 +1,15 @@
+﻿using Nestin.Core.Entities;
+
+namespace Nestin.Infrastructure.Data.Seeds
+{
+    static class GuestTypeSeed
+    {
+        public static List<GuestType> Data => new()
+        {
+            new GuestType {Id = 1, Name="Adults"},
+            new GuestType {Id = 2, Name="Childern"},
+            new GuestType {Id = 3, Name="Infants"},
+            new GuestType {Id = 4, Name="Pets"}
+        };
+    }
+}
