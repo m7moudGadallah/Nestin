d3c8402f2185aa51726b611c9f85a3bdb9a845c0
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 16:27:53 +0200

    feat(backend): add PropertyGuestReposiotry

---
+++ backend/nestin/Nestin.Core/Interfaces/IPropertyGuestRepository.cs
@@ -0,0 +1,11 @@
+﻿using Nestin.Core.Dtos;
+using Nestin.Core.Dtos.PropertyGuests;
+using Nestin.Core.Shared;
+
+namespace Nestin.Core.Interfaces
+{
+    public interface IPropertyGuestRepository
+    {
+        public Task<PaginatedResult<PropertyGuestsDto>> GetByPropertyId(string propertyId, GetAllQueryDto dto);
+    }
+}
