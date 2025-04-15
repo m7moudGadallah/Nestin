8358a75abb085dc9661bd5e91a92b2943d4eb36e
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 08:16:04 +0200

    feat(backend): add LocationMappingExtensions

---
+++ backend/nestin/Nestin.Core/Mappings/LocationMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Nestin.Core.Dtos.Locations;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class LocationMappingExtensions
+    {
+        public static LocationDto ToDo(this Location location)
+        {
+            return new LocationDto
+            {
+                Id = location.Id,
+                Name = location.Name,
+                CountryId = location.CountryId
+            };
+        }
+    }
+}
