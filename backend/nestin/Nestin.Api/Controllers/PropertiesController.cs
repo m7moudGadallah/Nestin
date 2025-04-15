de59cc8e1480167890d76bb859664a0e716fb8c0
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 09:53:15 +0200

    feat(backend): add PropertiesController

---
+++ backend/nestin/Nestin.Api/Controllers/PropertiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nestin.Core.Dtos.Properties;
+using Nestin.Core.Interfaces;
+
+namespace Nestin.Api.Controllers
+{
+    public class PropertiesController : BaseController
+    {
+        public PropertiesController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        { }
+
+        [HttpGet]
+        [EndpointSummary("Fetch all properties.")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetAll([FromQuery] FilterPropertyQueryParamsDto dto)
+        {
+            return Ok(await _unitOfWork.PropertyRepository.GetFilteredPropertiesAsync(dto));
+        }
+    }
+}
