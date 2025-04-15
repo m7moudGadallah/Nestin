5e2c084aff570101174403ec86622a5e6bd74e6e
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 16:05:40 +0200

    feat(backend): add PropertyAmenitiesDtos and mapping

---
+++ backend/nestin/Nestin.Core/Dtos/PropertyAmenities/PropertyAmenityDto.cs
@@ -0,0 +1,10 @@
+﻿using Nestin.Core.Dtos.Amenities;
+
+namespace Nestin.Core.Dtos.PropertyAmenities
+{
+    public class PropertyAmenityDto
+    {
+        public string PropertyId { get; set; }
+        public AmenityDto Amenity { get; set; }
+    }
+}
