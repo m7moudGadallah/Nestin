6bf2efe04719c6df2cf6e5e0f3631f5fb9f6f061
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 16:18:52 +0200

    feat(backend): add PropertyGuestDto and mapping

---
+++ backend/nestin/Nestin.Core/Dtos/PropertyGuests/PropertyGuestsDto.cs
@@ -0,0 +1,11 @@
+﻿using Nestin.Core.Dtos.GuestTypes;
+
+namespace Nestin.Core.Dtos.PropertyGuests
+{
+    public class PropertyGuestsDto
+    {
+        public string PropertyId { get; set; }
+        public GuestTypesDto GuestType { get; set; }
+        public int GuestCount { get; set; }
+    }
+}
