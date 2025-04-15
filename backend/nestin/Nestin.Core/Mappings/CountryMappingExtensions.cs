fc2462da8b903ec00954e47b7da7d1c22397738b
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 06:40:45 +0200

    feat(backend): add country dto mapping ext

---
+++ backend/nestin/Nestin.Core/Mappings/CountryMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Nestin.Core.Dtos.Countires;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class CountryMappingExtensions
+    {
+        public static CountryDto ToDo(this Country country)
+        {
+            return new CountryDto
+            {
+                Id = country.Id,
+                Name = country.Name,
+                RegionId = country.RegionId
+            };
+        }
+    }
+}
