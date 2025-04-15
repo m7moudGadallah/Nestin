fac31d6327f782564cc7b5a789af281926adc481
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 11:25:22 +0200

    feat(backend): add GetPropertyTypes endpoint

---
+++ backend/nestin/Nestin.Api/Controllers/PropertyTypesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nestin.Core.Dtos;
+using Nestin.Core.Dtos.PropertyTypes;
+using Nestin.Core.Interfaces;
+using Nestin.Core.Mappings;
+using Nestin.Core.Shared;
+
+namespace Nestin.Api.Controllers
+{
+    public class PropertyTypesController : BaseController
+    {
+        public PropertyTypesController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        { }
+
+        [HttpGet]
+        [EndpointSummary("Fetch all property types.")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllQueryDto queryDto)
+        {
+            var result = await _unitOfWork.PropertyTypeRepository.GetAllAsync(queryDto, q => q.OrderBy(x => x.Id));
+
+            var resultDto = new PaginatedResult<PropertyTypeDto>
+            {
+                Items = result.Items.Select(x => x.ToDto()).ToList(),
+                MetaData = result.MetaData
+            };
+
+            return Ok(resultDto);
+        }
+    }
+}
