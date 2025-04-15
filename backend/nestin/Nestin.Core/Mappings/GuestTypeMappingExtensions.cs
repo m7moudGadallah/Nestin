7670d791662e304ad0280fc5c58fb2fc79446064
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 15:17:06 +0200

    feat(backend): add GuestType dto and mapping

---
+++ backend/nestin/Nestin.Core/Mappings/GuestTypeMappingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Nestin.Core.Dtos.GuestTypes;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class GuestTypeMappingExtensions
+    {
+        public static GuestTypesDto ToDo(this GuestType guestType)
+        {
+            return new GuestTypesDto
+            {
+                Id = guestType.Id,
+                Name = guestType.Name
+            };
+        }
+    }
+}
