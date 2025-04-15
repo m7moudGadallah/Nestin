3a0d0aa19ea49089ea342c8ec1fe14abc024ab63
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 09:51:34 +0200

    feat(backend): ad Fitler Properity Dtos

---
+++ backend/nestin/Nestin.Core/Dtos/Properties/FilterPropertyQueryParamsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Nestin.Core.Dtos.Properties
+{
+    public class FilterPropertyQueryParamsDto : GetAllQueryDto
+    {
+        public int? LocationId { get; set; }
+        public int? GuestCount { get; set; }
+        public decimal? PriceMin { get; set; }
+        public decimal? PriceMax { get; set; }
+        public DateOnly? CheckIn { get; set; }
+        public DateOnly? CheckOut { get; set; }
+        public int? CountryId { get; set; }
+        public int? RegionId { get; set; }
+        public string? Sort { get; set; }
+    }
+}
---
+++ backend/nestin/Nestin.Core/Dtos/Properties/PropertyListItemDto.cs
@@ -0,0 +1,19 @@
+﻿using Nestin.Core.Dtos.Locations;
+using Nestin.Core.Dtos.PropertyPhotos;
+using Nestin.Core.Dtos.PropertyTypes;
+
+namespace Nestin.Core.Dtos.Properties
+{
+    public class PropertyListItemDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string OwnerId { get; set; }
+        public decimal PricePerNight { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public LocationDto Location { get; set; }
+        public PropertyTypeDto PropertyType { get; set; }
+        public PropertyPhotoDto? MainPhoto { get; set; }
+    }
+}
