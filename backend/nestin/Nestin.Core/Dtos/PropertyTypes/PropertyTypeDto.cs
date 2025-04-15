0ae86d1a8a249be8f1b1629d6cec6e1b653651b1
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 08:24:26 +0200

    feat(backend): add PropertyTypeDto

---
+++ backend/nestin/Nestin.Core/Dtos/PropertyTypes/PropertyTypeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Nestin.Core.Dtos.PropertyTypes
+{
+    public class PropertyTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Icon { get; set; }
+    }
+}
