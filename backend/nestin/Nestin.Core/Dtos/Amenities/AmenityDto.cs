b50722bf1e082bcb489e823def0d01c0f7c54bd1
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 14:48:29 +0200

    feat(backend): add AmentityDto and Mapping

---
+++ backend/nestin/Nestin.Core/Dtos/Amenities/AmenityDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Nestin.Core.Dtos.Amenities
+{
+    public class AmenityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Icon { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ backend/nestin/Nestin.Core/Mappings/AmenityMappingExtenstions.cs
@@ -0,0 +1,19 @@
+﻿using Nestin.Core.Dtos.Amenities;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class AmenityMappingExtenstions
+    {
+        public static AmenityDto ToDto(this Amenity amenity)
+        {
+            return new AmenityDto
+            {
+                Id = amenity.Id,
+                Name = amenity.Name,
+                Icon = amenity?.Icon,
+                CategoryId = amenity.CategoryId
+            };
+        }
+    }
+}
