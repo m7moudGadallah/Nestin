d29efb174cd5e17929e768408afaaf5a5419e21c
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-15 14:55:18 +0200

    feat(backend): add Amenity Repo

---
+++ backend/nestin/Nestin.Infrastructure/Repositories/AmenityRepository.cs
@@ -0,0 +1,12 @@
+﻿using Nestin.Core.Entities;
+using Nestin.Core.Interfaces;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure.Repositories
+{
+    public class AmenityRepository : GenericRepository<Amenity, int>, IAmenityRepository
+    {
+        public AmenityRepository(AppDbContext dbContext) : base(dbContext)
+        { }
+    }
+}
