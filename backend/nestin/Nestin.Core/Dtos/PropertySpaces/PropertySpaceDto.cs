1a4113ebc3adeb17370f967b320438fabc9544a8
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-16 08:03:43 +0200

    feat(backend): add PropertySpaceDto and mapping

---
+++ backend/nestin/Nestin.Core/Dtos/PropertySpaces/PropertySpaceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Nestin.Core.Dtos.PropertySpaces
+{
+    public class PropertySpaceDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsShared { get; set; } = false;
+        public int PropertySpaceTypeId { get; set; }
+        public string PropertyId { get; set; }
+    }
+}
---
+++ backend/nestin/Nestin.Core/Mappings/PropertySpaceMappingExtenstions.cs
@@ -0,0 +1,20 @@
+﻿using Nestin.Core.Dtos.PropertySpaces;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class PropertySpaceMappingExtenstions
+    {
+        public static PropertySpaceDto ToDto(this PropertySpace propertySpace)
+        {
+            return new PropertySpaceDto
+            {
+                Id = propertySpace.Id,
+                Name = propertySpace.Name,
+                IsShared = propertySpace.IsShared,
+                PropertySpaceTypeId = propertySpace.PropertySpaceTypeId,
+                PropertyId = propertySpace.PropertyId
+            };
+        }
+    }
+}
