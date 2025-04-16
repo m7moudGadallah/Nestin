71334f843e01bc4ccc57151333e1da12ce7a9358
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-16 06:20:37 +0200

    feat(backend): add PropertySpaceTypeController

---
+++ backend/nestin/Nestin.Api/PropertySpaceTypesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nestin.Api.Controllers;
+using Nestin.Core.Dtos;
+using Nestin.Core.Dtos.PropertySpaceTypes;
+using Nestin.Core.Interfaces;
+using Nestin.Core.Mappings;
+using Nestin.Core.Shared;
+
+namespace Nestin.Api
+{
+    public class PropertySpaceTypesController : BaseController
+    {
+        public PropertySpaceTypesController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        { }
+
+        [HttpGet]
+        [EndpointSummary("Fetch all property space types.")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(PaginatedResult<PropertySpaceTypeDto>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllQueryDto queryDto)
+        {
+            var result = await _unitOfWork.PropertySpaceTypeRepository.GetAllAsync(queryDto, q => q.OrderBy(x => x.Id));
+
+            var resultDto = new PaginatedResult<PropertySpaceTypeDto>
+            {
+                Items = result.Items.Select(x => x.ToDo()).ToList(),
+                MetaData = result.MetaData
+            };
+
+            return Ok(resultDto);
+        }
+    }
+}
