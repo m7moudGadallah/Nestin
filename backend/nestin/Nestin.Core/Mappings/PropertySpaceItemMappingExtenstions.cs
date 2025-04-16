d528768473f56fc868e1c79304e257bc9d539648
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-16 08:18:23 +0200

    feat(backend): add PropertySpaceItemDto and mapping

---
+++ backend/nestin/Nestin.Core/Mappings/PropertySpaceItemMappingExtenstions.cs
@@ -0,0 +1,19 @@
+﻿using Nestin.Core.Dtos.PropertySpaceItems;
+using Nestin.Core.Entities;
+
+namespace Nestin.Core.Mappings
+{
+    public static class PropertySpaceItemMappingExtenstions
+    {
+        public static PropertySpaceItemDto ToDto(this PropertySpaceItem propertySpaceItem)
+        {
+            return new PropertySpaceItemDto
+            {
+                Id = propertySpaceItem.Id,
+                PropertySpaceItemTypeId = propertySpaceItem.PropertySpaceItemTypeId,
+                PropertySpaceId = propertySpaceItem.PropertySpaceId,
+                Quantity = propertySpaceItem.Quantity
+            };
+        }
+    }
+}
