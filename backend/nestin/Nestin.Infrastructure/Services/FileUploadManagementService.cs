ed1b0bc8685aeebf99691e3dae0fb99d45419917
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-18 08:01:12 +0200

    feat(backend); add FileUploadManagementService

---
+++ backend/nestin/Nestin.Infrastructure/Services/FileUploadManagementService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Nestin.Core.Entities;
+using Nestin.Core.Interfaces;
+
+namespace Nestin.Infrastructure.Services
+{
+    class FileUploadManagementService : IFileUploadManagementService
+    {
+        private IUnitOfWork _unitOfWork;
+        private IFileStorageService _fileStorageService;
+        public FileUploadManagementService(IUnitOfWork unitOfWork, IFileStorageService fileStorageService)
+        {
+            _unitOfWork = unitOfWork;
+            _fileStorageService = fileStorageService;
+        }
+
+
+        public async Task<FileUpload> UploadAsync(IFormFile file)
+        {
+            var path = await _fileStorageService.SaveFileAsync(file);
+            var newFileUpload = new FileUpload
+            {
+                Path = path,
+            };
+
+            _unitOfWork.FileUploadRepository.Create(newFileUpload);
+            await _unitOfWork.FileUploadRepository.SaveChangesAsync();
+            return newFileUpload;
+        }
+
+        public async Task RemoveFileAsync(string fileUploadId)
+        {
+            var fileUplaod = await _unitOfWork.FileUploadRepository.GetByIdAsync(fileUploadId);
+
+            if (fileUplaod == null) return;
+
+            await _fileStorageService.DeleteFileAsync(fileUplaod.Path);
+        }
+    }
+}
