33048d320196e74437517106bf0633ea79f01cbc
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-22 12:35:13 +0200

    feat(frontend): add api-constant class

---
+++ frontend/nestin/src/utils/api-constant.util.ts
@@ -0,0 +1,34 @@
+export class ApiConstant {
+  public static baseUrl = 'https://localhost:7026/api/v1';
+
+  public static AccountsApi = {
+    register: `${ApiConstant.baseUrl}/accounts/register`,
+    login: `${ApiConstant.baseUrl}/accounts/login`,
+    logout: `${ApiConstant.baseUrl}/accounts/logout`,
+    'change-password': `${ApiConstant.baseUrl}/accounts/change-password`,
+  };
+
+  public static PropertiesApi = {
+    getAll: `${ApiConstant.baseUrl}/properties`,
+    getAllPropertyTypes: `${ApiConstant.baseUrl}/PropertyTypes`,
+  };
+
+  public static wishlist = {
+    wish: `${ApiConstant.baseUrl}/FavoriteProperties`,
+  };
+  public static booking = {
+    getAllBookings: `${ApiConstant.baseUrl}/Bookings`,
+    createBooking: `${ApiConstant.baseUrl}/Bookings`,
+  };
+  public static country = {
+    getAllCountries: `${ApiConstant.baseUrl}/Countries`,
+  };
+  public static region = {
+    getAllRegions: `${ApiConstant.baseUrl}/Regions`,
+  };
+  public static location = {
+    getAllLocations: `${ApiConstant.baseUrl}/Locations`,
+  };
+
+  // Add other grouped APIs here
+}
