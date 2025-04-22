f5423074410532060d8bee5526ddfdc86318b15a
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-22 14:51:58 +0200

    feat(frontend): init home page

--- frontend/nestin/src/app/app.routes.ts
+++ frontend/nestin/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'login', component: LoginPageComponent },
 ];
