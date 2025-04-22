88a69cd5c3532d74eb944729f62cb14c02854dc1
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-22 20:00:22 +0200

    feat(frontend): add footer

--- frontend/nestin/src/app/components/footer/footer.component.ts
+++ frontend/nestin/src/app/components/footer/footer.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import {
+  LucideAngularModule,
+  Copyright,
+  Facebook,
+  Twitter,
+  Instagram,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  readonly icons = {
+    copyright: Copyright,
+    facebook: Facebook,
+    twitter: Twitter,
+    instagram: Instagram,
+  };
+
+  supportLinks = [
+    { text: 'Help Center', url: '#' },
+    { text: 'Cancellation Options', url: '#' },
+    { text: 'Safety Information', url: '#' },
+  ];
+
+  nestinLinks = [
+    { text: 'About Us', url: '#' },
+    { text: 'Careers', url: '#' },
+    { text: 'Newsroom', url: '#' },
+  ];
+
+  legalLinks = [
+    { text: 'Terms', url: '#' },
+    { text: 'Privacy', url: '#' },
+    { text: 'Sitemap', url: '#' },
+  ];
+}
