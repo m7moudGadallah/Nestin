cab9209832bd2246afa975d6ed25ea4186d8b2ac
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-23 10:27:21 +0200

    feat(frontend): fix spinner in favorites page

--- frontend/nestin/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/nestin/src/app/layouts/main-layout/main-layout.component.ts
@@ -3,11 +3,16 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { Router } from 'lucide-angular';
 import { RouterOutlet } from '@angular/router';
-import { ToastContainerComponent } from "../../components/toast-container/toast-container.component";
+import { ToastContainerComponent } from '../../components/toast-container/toast-container.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, ToastContainerComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterOutlet,
+    ToastContainerComponent,
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
