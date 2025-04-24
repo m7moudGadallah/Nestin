fdd1be96f97d8ba71fac1aca78547104f1f85793
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-24 17:36:14 +0200

    feat(backend): add HostUpgradeRequestRepository

--- backend/nestin/Nestin.Core/Interfaces/IHostUpgradeRequestRepository.cs
+++ backend/nestin/Nestin.Core/Interfaces/IHostUpgradeRequestRepository.cs
@@ -1,9 +1,12 @@
-﻿using Nestin.Core.Entities;
+﻿using Nestin.Core.Dtos.HostUpgradeRequests;
+using Nestin.Core.Entities;
+using Nestin.Core.Shared;
 
 namespace Nestin.Core.Interfaces
 {
     public interface IHostUpgradeRequestRepository : IGenericRepository<HostUpgradeRequest, string>
     {
+        public Task<PaginatedResult<HostUpgradeRequestDto>> GetAllAsync(HostUpgradeRequestFilterQueryParamsDto queryDto);
         public Task<HostUpgradeRequest?> GetPendingRequestByUserIdAsync(string userId);
         public Task<HostUpgradeRequest?> GetLastRequestByUserIdASync(string userId);
     }
--- backend/nestin/Nestin.Infrastructure/Repositories/HostUpgradeRequestRepository.cs
+++ backend/nestin/Nestin.Infrastructure/Repositories/HostUpgradeRequestRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Nestin.Core.Dtos.HostUpgradeRequests;
 using Nestin.Core.Entities;
 using Nestin.Core.Interfaces;
+using Nestin.Core.Mappings;
+using Nestin.Core.Shared;
 using Nestin.Infrastructure.Data;
 
 namespace Nestin.Infrastructure.Repositories
@@ -10,6 +13,60 @@ namespace Nestin.Infrastructure.Repositories
         public HostUpgradeRequestRepository(AppDbContext dbContext) : base(dbContext)
         { }
 
+        public async Task<PaginatedResult<HostUpgradeRequestDto>> GetAllAsync(HostUpgradeRequestFilterQueryParamsDto queryDto)
+        {
+            var query = _dbContext.HostUpgradeRequests
+                .Include(x => x.FrontPhoto)
+                .Include(x => x.BackPhoto)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(queryDto.UserId))
+                query = query.Where(x => x.UserId == queryDto.UserId);
+
+            if (!string.IsNullOrEmpty(queryDto.Status))
+            {
+                var status = Enum.Parse<HostUgradeRequestStatus>(queryDto.Status, ignoreCase: true);
+                query = query.Where(x => x.Status == status);
+            }
+
+            if (!string.IsNullOrEmpty(queryDto.ApprovedBy))
+                query = query.Where(x => x.ApprovedBy == queryDto.ApprovedBy);
+
+            if (queryDto.ApprovalDate is not null)
+                query = query.Where(x => x.ApprovalDate == queryDto.ApprovalDate);
+
+            if (!string.IsNullOrEmpty(queryDto.DocumentNumber))
+                query = query.Where(x => x.DocumentNumber == queryDto.DocumentNumber);
+
+            query = queryDto.Sort switch
+            {
+                "status" => query.OrderBy(x => x.Status),
+                "created_at_asc" => query.OrderBy(x => x.CreatedAt),
+                "created_at_desc" => query.OrderByDescending(x => x.CreatedAt
+                ),
+                "approval_date_asc" => query.OrderBy(x => x.ApprovalDate),
+                "approval_date_desc" => query.OrderByDescending(x => x.ApprovalDate),
+                _ => query.OrderBy(x => x.CreatedAt),
+            };
+
+            var total = await query.CountAsync();
+            var items = await query
+                .Skip(queryDto.CalcSkippedItems())
+                .Take(queryDto.PageSize)
+                .Select(x => x.ToDto())
+                .ToListAsync();
+
+            return new PaginatedResult<HostUpgradeRequestDto>
+            {
+                Items = items,
+                MetaData = new PaginationMetaData
+                {
+                    Page = queryDto.Page,
+                    PageSize = queryDto.PageSize,
+                    Total = total
+                }
+            };
+        }
         public async Task<HostUpgradeRequest?> GetLastRequestByUserIdASync(string userId)
         {
             return await _dbContext.HostUpgradeRequests
