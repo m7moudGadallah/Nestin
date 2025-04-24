ef4c5c3152b169617b031bd8b8c1f36850480a22
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-24 14:57:48 +0200

    feat(backend): add valdiation attributes for dtos

---
+++ backend/nestin/Nestin.Core/Validation/ValidHostUpgradeDocumentTypeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Nestin.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nestin.Core.Validation
+{
+    public class ValidHostUpgradeDocumentTypeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("DocumentType is required.");
+            }
+
+            var documentType = value.ToString();
+            if (Enum.TryParse(typeof(HostUpgradeRequestDocumentType), documentType, true, out _))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult($"Invalid DocumentType. Allowed values are: {string.Join(", ", Enum.GetNames(typeof(HostUpgradeRequestDocumentType)))}");
+        }
+    }
+}
---
+++ backend/nestin/Nestin.Core/Validation/ValidHostUpgradeRequestStatusAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Nestin.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nestin.Core.Validation
+{
+    public class ValidHostUgradeRequestStatusAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Assume null/empty checks are handled by [Required] if needed
+            if (value is string stringValue)
+            {
+                if (Enum.TryParse(typeof(HostUgradeRequestStatus), stringValue, true, out _))
+                    return ValidationResult.Success;
+            }
+            else if (value != null && Enum.IsDefined(typeof(HostUgradeRequestStatus), value))
+            {
+                return ValidationResult.Success;
+            }
+
+            // Invalid case
+            var allowedValues = string.Join(", ", Enum.GetNames(typeof(HostUgradeRequestStatus)));
+            return new ValidationResult($"Must be one of: {allowedValues}");
+        }
+    }
+}
