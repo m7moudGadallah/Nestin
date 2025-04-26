3864b54f4092b5e7a5edd57c16ec1ab56a4e362f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 14:50:39 +0300

    feat(backend): update PropertyGuestRepository

--- backend/nestin/Nestin.Core/Interfaces/IPropertyGuestRepository.cs
+++ backend/nestin/Nestin.Core/Interfaces/IPropertyGuestRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Nestin.Core.Dtos;
 using Nestin.Core.Dtos.PropertyGuests;
+using Nestin.Core.Entities;
 using Nestin.Core.Shared;
 
 namespace Nestin.Core.Interfaces
@@ -7,5 +8,9 @@ namespace Nestin.Core.Interfaces
     public interface IPropertyGuestRepository
     {
         public Task<PaginatedResult<PropertyGuestsDto>> GetByPropertyIdAsync(string propertyId, GetAllQueryDto dto);
+        public Task<PropertyGuest?> GetByPropertyAndGuestTypeAsync(string propertyId, int guestTypeId);
+        public void Create(PropertyGuest entity);
+        public void Update(PropertyGuest entity);
+        public void Delete(PropertyGuest entity);
     }
 }
--- backend/nestin/Nestin.Infrastructure/Repositories/PropertyGuestRepository.cs
+++ backend/nestin/Nestin.Infrastructure/Repositories/PropertyGuestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Nestin.Core.Dtos;
 using Nestin.Core.Dtos.PropertyGuests;
+using Nestin.Core.Entities;
 using Nestin.Core.Interfaces;
 using Nestin.Core.Mappings;
 using Nestin.Core.Shared;
@@ -38,6 +39,28 @@ namespace Nestin.Infrastructure.Repositories
                     PageSize = dto.PageSize
                 }
             };
+        }
+
+        public async Task<PropertyGuest?> GetByPropertyAndGuestTypeAsync(string propertyId, int guestTypeId)
+        {
+            return await _dbContext.PropertyGuests
+               .Include(x => x.GuestType)
+               .Where(x => x.PropertyId == propertyId && x.GuestTypeId == guestTypeId)
+               .FirstOrDefaultAsync();
+        }
+
+        public void Create(PropertyGuest entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public void Update(PropertyGuest entity)
+        {
+            _dbContext.Update(entity);
+        }
+        public void Delete(PropertyGuest entity)
+        {
+            _dbContext.Remove(entity);
         }
     }
 }
