f39e98fae310d62a3df714382e7e47d77f6b4075
Kimo1928 <kareemmohamed88813@gmail.com>
2025-04-26 15:24:39 +0300

    Adding BookingHistory

---
+++ frontend/nestin/src/app/pages/booking-history-page/booking-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-history',
+  imports: [],
+  templateUrl: './booking-history.component.html',
+  styleUrl: './booking-history.component.scss'
+})
+export class BookingHistoryComponent {
+
+}
