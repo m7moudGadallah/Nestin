b7556293fd8959d9601fec2234c26bb53afb7924
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 14:36:45 +0300

    feat(backend): init guest controller

---
+++ backend/nestin/Nestin.Api/Controllers/PropertyGuestsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nestin.Core.Dtos.PropertyGuests;
+using Nestin.Core.Interfaces;
+
+namespace Nestin.Api.Controllers
+{
+    [Authorize(Roles = "Host,Admin")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public class PropertyGuestsController : BaseController
+    {
+        public PropertyGuestsController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        { }
+
+        [HttpPost]
+        [EndpointSummary("Create Property Guest.")]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(PropertyGuestsDto), StatusCodes.Status201Created)]
+        public Task<IActionResult> Create([FromBody] PropertyGuestCreateDto dto)
+        {
+            return Task.FromResult<IActionResult>(NotImplementedResponse());
+        }
+
+        [HttpPatch]
+        [EndpointSummary("Update Property Guest.")]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(PropertyGuestsDto), StatusCodes.Status200OK)]
+        public Task<IActionResult> Update([FromBody] PropertGuestUpdateDto dto)
+        {
+            return Task.FromResult<IActionResult>(NotImplementedResponse());
+        }
+
+        [HttpDelete("{propertyId}/{guestTypeId}")]
+        [EndpointSummary("delete an existing Property Guest.")]
+        public Task<IActionResult> Delete([FromRoute] string propertyId, [FromRoute] string guestTypeId)
+        {
+            return Task.FromResult<IActionResult>(NotImplementedResponse());
+        }
+    }
+}
