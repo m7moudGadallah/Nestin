15b0d4d2cf6b36c289c1772c1eb49132a9e604ea
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 09:45:33 +0300

    feat(backend): update LoggedInUser

--- backend/nestin/Nestin.Core/Dtos/Accounts/LoggedInUser.cs
+++ backend/nestin/Nestin.Core/Dtos/Accounts/LoggedInUser.cs
@@ -5,5 +5,13 @@
         public string Id { get; init; }
         public string UserName { get; init; }
         public List<string> Roles { get; init; } = new List<string>();
+
+        public bool IsInRole(string role)
+        {
+            if (string.IsNullOrWhiteSpace(role))
+                return false;
+
+            return Roles.Contains(role, StringComparer.OrdinalIgnoreCase);
+        }
     }
 }
