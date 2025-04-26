29cb175c4fe95f288d94a27e08b90332dd6bb67e
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 13:00:54 +0300

    feat(backend): update PropertyPhotosReorderDto

--- backend/nestin/Nestin.Core/Dtos/PropertyPhotos/PropertyPhotosReorderDto.cs
+++ backend/nestin/Nestin.Core/Dtos/PropertyPhotos/PropertyPhotosReorderDto.cs
@@ -5,6 +5,9 @@ namespace Nestin.Core.Dtos.PropertyPhotos
     public class PropertyPhotosReorderDto
     {
         [Required]
+        public string PropertyId { get; set; }
+
+        [Required]
         public List<string> PhotoIds { get; set; }
     }
 }
