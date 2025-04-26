761a70dba03e50211c12eafe3aa8791148b43efe
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 09:46:08 +0300

    feat(backend): add PropertyUpdateDto

--- backend/nestin/Nestin.Core/Dtos/Properties/PropertyUpdateDto.cs
+++ backend/nestin/Nestin.Core/Dtos/Properties/PropertyUpdateDto.cs
@@ -2,29 +2,28 @@
 
 namespace Nestin.Core.Dtos.Properties
 {
-    public class PropertyCreateDto
+    public class PropertyUpdateDto
     {
-        [Required(ErrorMessage = "Title is required")]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         [StringLength(2000, ErrorMessage = "Description cannot exceed 2000 characters")]
         public string? Description { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Property type is required")]
-        public int PropertyTypeId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Property type must be greater than or equal 1")]
+        public int? PropertyTypeId { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Location is required")]
-        public int LocationId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Location must be greater than or equal 1")]
+        public int? LocationId { get; set; }
 
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
-        public decimal PricePerNight { get; set; }
+        public decimal? PricePerNight { get; set; }
 
         [Range(-90, 90, ErrorMessage = "Latitude must be between -90 and 90")]
-        public decimal Latitude { get; set; }
+        public decimal? Latitude { get; set; }
 
         [Range(-180, 180, ErrorMessage = "Longitude must be between -180 and 180")]
-        public decimal Longitude { get; set; }
+        public decimal? Longitude { get; set; }
 
         [StringLength(1000, ErrorMessage = "Safety info cannot exceed 1000 characters")]
         public string? SafteyInfo { get; set; }
@@ -34,5 +33,7 @@ namespace Nestin.Core.Dtos.Properties
 
         [StringLength(1000, ErrorMessage = "Cancellation policy cannot exceed 1000 characters")]
         public string? CancellationPolicy { get; set; }
+
+        public bool? IsActive { get; set; }
     }
 }
