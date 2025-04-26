efda3a6e9d6119eef71b18cc12dbba08b22d82a6
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 12:39:58 +0300

    feat(backend): add PropertyPhotoRepository

---
+++ backend/nestin/Nestin.Infrastructure/Repositories/PropertyPhotoRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nestin.Core.Entities;
+using Nestin.Core.Interfaces;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure.Repositories
+{
+    class PropertyPhotoRepository : BaseRepository, IPropertyPhotoRepository
+    {
+        public PropertyPhotoRepository(AppDbContext dbContext) : base(dbContext)
+        { }
+
+        public void Create(PropertyPhoto entity)
+        {
+            _dbContext.Add(entity);
+        }
+
+        public async Task<IReadOnlyCollection<PropertyPhoto>> GetAllByPropertyIdASync(string propertyId)
+        {
+            var query = _dbContext.PropertyPhotos
+                .OrderBy(x => x.TouchedAt)
+                .Where(x => x.PropertyId == propertyId)
+                .AsQueryable();
+
+            var items = await query.ToListAsync();
+            return items;
+        }
+
+        public void Update(PropertyPhoto entity)
+        {
+            _dbContext.Update(entity);
+        }
+    }
+}
