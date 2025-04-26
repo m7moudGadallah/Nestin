819debfee6faf627d97de56fecb2fb9672e184b9
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-26 14:36:31 +0300

    feat(backend): add Create and Update dtos for PropertyGuest

---
+++ backend/nestin/Nestin.Core/Dtos/PropertyGuests/PropertGuestUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nestin.Core.Dtos.PropertyGuests
+{
+    public class PropertGuestUpdateDto
+    {
+        [Required]
+        public string PropertyId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "GuestTypeId must be greater than 1")]
+        public int GuestTypeId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "GuestCount must be greater than 1")]
+        public int GuestCount { get; set; }
+    }
+}
+
---
+++ backend/nestin/Nestin.Core/Dtos/PropertyGuests/PropertyGuestCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nestin.Core.Dtos.PropertyGuests
+{
+    public class PropertyGuestCreateDto
+    {
+        [Required]
+        public string PropertyId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "GuestTypeId must be greater than 1")]
+        public int GuestTypeId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "GuestCount must be greater than 1")]
+        public int GuestCount { get; set; }
+    }
+}
