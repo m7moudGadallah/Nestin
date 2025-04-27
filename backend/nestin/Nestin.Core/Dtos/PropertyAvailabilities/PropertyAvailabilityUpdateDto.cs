171409864d4d76432af24ba0911e91b0ea2500e0
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-04-27 11:44:08 +0300

    feat(backend): add PropertyAvailability Create and Update dtos

---
+++ backend/nestin/Nestin.Core/Dtos/PropertyAvailabilities/PropertyAvailabilityUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nestin.Core.Dtos.PropertyAvailabilities
+{
+    public class PropertyAvailabilityUpdateDto
+    {
+        [Required]
+        public string PropertyId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
