57f13223f09e94aee648ae58a6b6924cb7530985
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-05-01 15:59:51 +0300

    feat(backend): add ReviewRepository

---
+++ backend/nestin/Nestin.Core/Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using Nestin.Core.Dtos;
+using Nestin.Core.Dtos.Reviews;
+using Nestin.Core.Entities;
+using Nestin.Core.Shared;
+
+namespace Nestin.Core.Interfaces
+{
+    public interface IReviewRepository : IGenericRepository<Review, string>
+    {
+        public Task<PaginatedResult<ReviewDto>> GetPropertyReviews(string propertyId, GetAllQueryDto dto);
+    }
+}
---
+++ backend/nestin/Nestin.Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nestin.Core.Dtos;
+using Nestin.Core.Dtos.Reviews;
+using Nestin.Core.Entities;
+using Nestin.Core.Interfaces;
+using Nestin.Core.Mappings;
+using Nestin.Core.Shared;
+using Nestin.Infrastructure.Data;
+
+namespace Nestin.Infrastructure.Repositories
+{
+    class ReviewRepository : GenericRepository<Review, string>, IReviewRepository
+    {
+        public ReviewRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<PaginatedResult<ReviewDto>> GetPropertyReviews(string propertyId, GetAllQueryDto dto)
+        {
+            var query = _dbContext.Reviews
+                .Include(x => x.Booking)
+                .ThenInclude(x => x.User)
+                .ThenInclude(x => x.UserProfile)
+                .ThenInclude(x => x.Photo)
+                .Include(x => x.Booking)
+                .ThenInclude(x => x.Property)
+                .Where(x => x.Booking.PropertyId == propertyId)
+                .AsQueryable();
+
+            var total = await query.CountAsync();
+
+            var items = await query
+                .Skip(dto.CalcSkippedItems())
+                .Take(dto.PageSize)
+                .Select(x => x.ToDto())
+                .ToListAsync();
+
+            return new PaginatedResult<ReviewDto>
+            {
+                Items = items,
+                MetaData = new PaginationMetaData
+                {
+                    Page = dto.Page,
+                    PageSize = dto.PageSize,
+                    Total = total,
+                }
+            };
+        }
+    }
+}
